fdc621487ff4e528a1850dd3a09be783ac8e7262
Mizhanur-Rahman <dev.mizhan@gmail.com>
2023-10-28 13:27:55 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll(".button");
+
+let string = "";
+let arr = Array.from(buttons);
+
+function calculate(button) {
+    const value = button.textContent;
+
+    if(value == '=') {
+        string = eval(string);
+        input.value = string;
+    }
+    else if(value == 'AC') {
+        string = "";
+        input.value = string;
+    }
+    else if(value == "DEL") {
+        string = string.substring(0, string.length - 1);
+        input.value = string;
+    }
+     else{
+        string = string + value;
+        input.value = string;
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
